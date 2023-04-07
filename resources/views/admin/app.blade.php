0d538dde153fce7e3b00e46f5254b79e1d4f4256
ArunBalaji06 <arunbalaji@sparkouttech.com>
2023-04-07 15:54:07 +0530

    files added

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>@yield('title')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -14,6 +14,7 @@
           integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 <body class="antialiased bg-light">
+
     @yield('content')
 </body>
 
@@ -41,39 +42,7 @@
     swal("Success!", 'Login successfull');
     @endif
 
-    $(document).on('click', '#delete_all_notifications', function(){
-        getAjaxRequest('{{ url('admin/notification/delete') }}')
-    })
-
-    function getAjaxRequest(url) {
-        $.ajax({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            url : url,
-            type : 'GET',
-            dataType : 'json',
-            success : function(result){
-            }
-        });
-    }
-
-    $(document).ready(function(){
-        let ip_address = '127.0.0.1';
-        let socket_port = '3000';
-        let socket = io(ip_address+ ':' +socket_port);
-        socket.on('houseOwnerCreatedNotification', (message) => {
-            var oldValue = {{ getNotificationsCount() }};
-            var newValue = 1;
-            console.log('getNotificationsCount', {{ getNotificationsCount() }});
-            console.log('oldValue + newValue',oldValue + newValue)
-            $('#notification_count').text( oldValue + newValue);
-        });
-    })
-
-
-
-
-
 </script>
+@yield('script')
+
 </html>
